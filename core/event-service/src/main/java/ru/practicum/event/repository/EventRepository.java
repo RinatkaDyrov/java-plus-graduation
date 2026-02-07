bc059aeb775c8a5ef3078b24df464f2307fb50ae
rinatka_dyrov <poalroid264@yandex.ru>
2026-02-07 15:46:55 +0300

    recommendations CollectorService done (with tests) Aggregator done  (with tests) Analyzer done (with tests) use kafka and take some fun forgot about some clients

--- core/event-service/src/main/java/ru/practicum/event/repository/EventRepository.java
+++ core/event-service/src/main/java/ru/practicum/event/repository/EventRepository.java
@@ -25,20 +25,6 @@ public interface EventRepository extends JpaRepository<Event, Long> {
 
     List<Event> findAll(Specification<Event> spec, Pageable pageable);
 
-    @Modifying(clearAutomatically = true, flushAutomatically = true)
-    @Query("UPDATE Event e SET e.views = e.views + 1 WHERE e.id IN :ids")
-    void incrementViews(@Param("ids") List<Long> ids);
-
-    @Modifying(clearAutomatically = true, flushAutomatically = true)
-    @Query("UPDATE Event e SET e.views = :views WHERE e.id = :eventId")
-    void updateViews(@Param("eventId") Long eventId,
-                     @Param("views") int views);
-
-    @Modifying(clearAutomatically = true, flushAutomatically = true)
-    @Query("UPDATE Event e SET e.confirmedRequests = :confirmedRequests WHERE e.id = :eventId")
-    void updateConfirmedRequests(@Param("eventId") Long eventId,
-                                 @Param("confirmedRequests") int confirmedRequests);
-
     boolean existsByCategoryId(Long categoryId);
 
     @Modifying(clearAutomatically = true, flushAutomatically = true)
